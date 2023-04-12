79752f7924aa134aaf91790952b008fd7618aa11
WiseMan <45821875+TheWiseMan@users.noreply.github.com>
2023-04-12 11:51:43 +0200

    Create update_variables.cmd

---
+++ client/update_variables.cmd
@@ -0,0 +1,15 @@
+set "wisepackJavaRep=https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip"
+set "wisepackForgeRep=https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.43/forge-1.19.4-45.0.43-installer.jar"
+@echo off
+echo [96m
+echo Downloading JAVA17.
+echo [37m
+curl -o java.zip https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip
+echo [92m Downloaded.
+
+echo [96m
+echo Downloading FORGE .
+echo [37m
+curl -o java.zip https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip
+echo [92m Downloaded.
+pause
